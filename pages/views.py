c06e834c19a8dda2d33797a8b75429692e23f9dd
gauravtechies <gauravkamboj13121992@gmail.com>
2019-06-19 16:00:18 +0530

    dynamic routes and url config

--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,20 @@ def about(request):
 class contact(View):
     def get(self,request):
         return HttpResponse("<h1> Contact us </h1>")
+
+def member(request,id):        
+    return HttpResponse("<h1>Team member  id:{}</h1>".format(id)) 
+
+def team(request):
+    return HttpResponse("<h1>Team members</h1>")
+
+
+def categoryWiseMember(request,cat_id,mem_id):
+    return HttpResponse("<h1>Memberid: {} categoryid: {}</h1>".format(cat_id,mem_id))
+
+#dynamic routes if cat_id and mem_id exist 
+def greatmember(request):
+    if(request.method=='GET' and 'cat_id' in request.GET and 'mem_id' in request.GET):
+        return HttpResponse("<h1>Memberid: {} categoryid: {}</h1>".format(request.GET.get('mem_id'),request.GET.get('cat_id')))
+    else:
+        return HttpResponse("<h1>All Great members</h1>")
